a1b8a1dbe0fd6e43ef4c3afe8a081ad0bd901e1b
xhoi700 <10057@holbertonstudents.com>
2024-10-22 20:16:14 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++);
+{
+
+if (letter != 'e' && letter != 'q');
+putchar(letter);
+putchar('\n');
+
+return (0);
+
+}
+
+
+}
