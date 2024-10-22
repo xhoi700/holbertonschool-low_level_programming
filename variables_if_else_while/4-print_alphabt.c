a7685d0f47997cbacd31734efd00fc2c924573c6
xhoi700 <10057@holbertonstudents.com>
2024-10-22 20:31:56 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,16 @@
  *
  * Return: Always 0 (Success).
  */
-int main(void)
-{
-char letter;
-
-for (letter = 'a'; letter <= 'z'; letter++);
+int main(void) 
 {
 
-if (letter != 'e' && letter != 'q');
-putchar(letter);
-putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++) {
+        if (c != 'e' && c != 'q') {
+            putchar(c);
+        }
+    }
 
-return (0);
-
-}
-
-
+    putchar('\n');
+    return 0;
 }
