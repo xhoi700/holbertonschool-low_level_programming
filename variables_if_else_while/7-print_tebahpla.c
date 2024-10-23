64577476ce1c4e8739ae1e80fd4767c19480dcef
xhoi700 <10057@holbertonstudents.com>
2024-10-23 17:46:26 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char revalpha[] = "zyxwvutsrqponmlkjihgfedcba";
+	int x;
+
+	for (x = 0; x < 26; x++)
+{
+	putchar(revalpha[x]);
+}
+	putchar('\n');
+
+	return (0);
+}
