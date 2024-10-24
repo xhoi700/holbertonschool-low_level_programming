063a3681356afb939be217178bf0af7777c3a5bd
xhoi700 <10057@holbertonstudents.com>
2024-10-24 21:21:05 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
-/*
-* print_sign - sxvgfg
-*/
+/**
+ * print_sign - used to print sign
+ * @n: the character to check
+ * Return: 1 n is greater than zero, 0 if n is zero, -1 if n is less then zero
+ */
 
-int print_sign(int n)
+	int print_sign(int n)
+
 {
+
 	if (n > 0)
 	{
 		_putchar('+');
-
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('=');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar ('-');
+		_putchar('-');
 		return (-1);
-
 	}
+
 }
