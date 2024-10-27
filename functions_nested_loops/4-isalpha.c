8f01d1d870ca7f0292edbc18efafe18355dfd413
xhoi700 <10057@holbertonstudents.com>
2024-10-27 17:04:15 +0000

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,15 @@
-#include "main.h"
-/*
-* print_isalpha - sxvgfg
-*/
+#include"main.h"
+
+/**
+ * _isalpha - checks for an alphabetic character; in the standard "C" locale
+ * @c: the character to check
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
 
 int _isalpha(int c)
+
 {
-	if ((a <= c && z >= c) || (A <= c && Z >= c))
+	if ((97 <= c &&  122 >= c) || (65 <= c && 90 >= c))
 	{
 		return (1);
 	}
