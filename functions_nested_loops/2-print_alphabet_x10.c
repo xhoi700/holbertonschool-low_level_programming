34be9c254902c101f7600021a18ea72fddcf1fb3
xhoi700 <10057@holbertonstudents.com>
2024-10-28 18:22:07 +0000

    2-alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - Entry point
+ *  * print_alphabet_x10 - Entry point
+ *   * Return:0
  */
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c;
+	int i;
 
+	for (i = 0; i < 10; i++)
+{
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
+}
 }
