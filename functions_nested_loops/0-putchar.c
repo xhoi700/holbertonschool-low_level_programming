e2660afd3396ca982eae3bad54241a33590db1b2
xhoi700 <10057@holbertonstudents.com>
2024-10-28 09:42:20 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
-/**
- *  *  * main - entry point
- *   *  * Return: 0
- */
+#include "main.h"
+
 int main(void)
-{
-	printf("_putchar\n");
-	return (0);
+{ 
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    
+    return(0);
 }
