dcbb0b7850d0964b56ef3a46622fb9d04df96875
xhoi700 <10057@holbertonstudents.com>
2024-10-29 20:09:19 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
- * print_line - draws a straight line
+ * print_line - function print line
+ * @n: - parameter from input until when print _
  * main.h header file
- * @n number of times
+ * Ose direkt fillo ciklin me 1 pa kushtet if / else
+ * Return:0
  */
 
 void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
@@ -21,5 +20,7 @@ void print_line(int n)
 		}
 		_putchar('\n');
 	}
-
+	else
+		_putchar('\n');
 }
+
