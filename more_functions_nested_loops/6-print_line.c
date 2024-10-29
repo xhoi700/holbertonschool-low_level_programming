1ebdb6dc6cd85d55aac7b9d2ecb9c8218fd922dc
xhoi700 <10057@holbertonstudents.com>
2024-10-29 12:03:50 +0000

    6-lines.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * main.h header file
+ * Return:0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
