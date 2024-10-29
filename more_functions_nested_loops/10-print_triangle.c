a961d10a08875b9fc3a0955bf17e5d2a00a89e4e
xhoi700 <10057@holbertonstudents.com>
2024-10-29 21:05:53 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_triangle - function print_triangle
+ * @size: - parameter from input until when print #
+ * main.h header file
+ * Return: Void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	{
+		for
+	}
+}
--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,24 @@
 #include "main.h"
-
 /**
- * print_square - function print square
- * @size: - parameter from input until when print #
+ * print_square - function print line
+ * @size: - parameter from input until when print \
  * main.h header file
  * Return: Void
  */
 
 void print_square(int size)
 {
-	int i;
-	int j;
+	int i, j;
 
-	if (size <= 0 )
-	print_square('\n');
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j =  0; j < size; i++)
-			_putchar('#');
-		_putchar('\n');
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
