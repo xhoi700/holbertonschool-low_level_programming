1fa11fb0ec15ee1c851f8dee415028d88cbbb8e6
xhoi700 <10057@holbertonstudents.com>
2024-10-29 20:39:36 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function print line
+ * @n: - parameter from input until when print \
+ * main.h header file
+ * Return: Void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
