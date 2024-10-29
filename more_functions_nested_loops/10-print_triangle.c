0407271871e915e18d42628b5221fad4c7b67967
xhoi700 <10057@holbertonstudents.com>
2024-10-29 21:31:27 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,17 +1,29 @@
 #include "main.h"
-
 /**
- * print_triangle - function print_triangle
- * @size: - parameter from input until when print #
- * main.h header file
- * Return: Void
+ * print_triangle - print 90deg triangle
+ * @size: input from user until when to print #
+ * i,j,k for iterations cycles
+ * Return: 0
  */
-
 void print_triangle(int size)
 {
-	int i;
-	int j;
+	int i, j, k;
+
+	if (size > 0)
 	{
-		for
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
