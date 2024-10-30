a250f7fb25d37b8be8dcb4824016f9eccde56015
xhoi700 <10057@holbertonstudents.com>
2024-10-30 11:05:06 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * reset - updates the value
- *@n: is parameter
+ * reset_to_98 - dasdas
+ * @n: is a parameter
+ *
  */
 
 void reset_to_98(int *n)
 {
-	n * = 98
+	*n = 98;
 }
