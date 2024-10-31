f5bbf4205b1c6e33a141575dbc2da499ded79738
xhoi700 <10057@holbertonstudents.com>
2024-10-31 13:42:31 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string,
+ *@str: dtrfuy
+ */
+
+void puts_half(char *str)
+
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+	if( i / 2 == 0)
+	_putchar(str[i]);
+	{
+		if
+	}
