6591c3d45b294565e1d686631997be23c391a488
xhoi700 <10057@holbertonstudents.com>
2024-11-01 13:33:39 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcmp - compares two strings
- * @s1: points value to a string
- * @s2: points value to a string
- */
+* _strcmp - compares two strings
+* @s1: first string to compare
+* @s2: second string to compare
+* Return: diff of two characters
+*/
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int diff = 0;
+	int difference = 0;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			diff = s1[i] - s2[i];
+			difference = s1[i] - s2[i];
 			break;
 		}
 		i++;
 	}
-	return (diff);
+	return (difference);
 }
