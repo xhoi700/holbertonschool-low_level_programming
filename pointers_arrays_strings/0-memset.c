6f6f66f4e934550c45b8dea2445ce5d1d206de6b
xhoi700 <10057@holbertonstudents.com>
2024-11-04 10:43:20 +0000

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *_memset - sdda
- *@s: ssfd
- *@b: szda
- *@n: aedsc
- *Return: sedd
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
