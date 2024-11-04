29bc12d2fc5a7ca6468b819695c4211b94a0ddea
xhoi700 <10057@holbertonstudents.com>
2024-11-04 10:51:37 +0000

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - filles memory with byte
+ * @dest: points to the memory
+ * @src: points to source
+ * @n: number of times to insert the char
  *
- * Return: Nothing.
+ * Return: (dest)
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
