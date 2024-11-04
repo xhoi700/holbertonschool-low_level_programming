13077f81d4f5080179ddcf0bf50f074864cf7d3d
xhoi700 <10057@holbertonstudents.com>
2024-11-04 11:46:22 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,20 +2,23 @@
 #include <stddef.h>
 
 /**
-*_strchr - checks for chars
-* @s: points to the string
-* @c: char to check
-* Return: (s + 1)
-*/
+ * _strchr - functions to search a char in str
+ * @s: pointer to string
+ * @c: character to fill
+ * Return: string
+ */
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i != '\0'; i++)
+	if (c == '\0')
+		return ("");
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		return (s + i);
+			return (s + i);
 	}
 	return (NULL);
 }
