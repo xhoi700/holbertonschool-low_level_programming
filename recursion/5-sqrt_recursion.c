2600ba5674ab32826497343d364a7b2f5aa97c35
xhoi700 <10057@holbertonstudents.com>
2024-11-07 11:44:33 +0000

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+*_sqrt_primary - primary recursive function
+*@n: number to check
+*@r: the root number
+Return: (r)
+*/
+int _sqrt_primary(int n, int r)
+{
+	if (r * r == n)
+	{
+		return (r);
+	}
+	if (r * r > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_primary(n, r + 1));
+}
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_primary(n, 0));
+}
