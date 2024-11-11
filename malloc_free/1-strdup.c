59dceb970855b0686eb615c04bdcb6b6dfa0e822
xhoi700 <10057@holbertonstudents.com>
2024-11-11 20:20:23 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,30 +1,33 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
-* create_array - creates an array of chars
-* @size: the size of memory to print
-* @c: the adress of memory to print
-* Return: nothing
-*/
+ * _strdup - duplicate a string
+ * @str: string to be duplicated.
+ * Return: pointer to the duplicated string, or NULL if insufficient memory
+ * was available.
+ */
 
-
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-	char *array;
-	unsigned int i;
+	char *tmp;
+	size_t len;
+	int i;
 
-	if (size == 0)
-		return (NULL);
-
-	array = malloc(size * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
+	if (str == NULL)
 	{
-		array[i] = c;
+		return (NULL);
 	}
-	return (array);
+	len = strlen(str) + 1;
+	tmp = malloc(len);
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (int) len; i++)
+		tmp[i] = str[i];
+	tmp[i] = '\0';
+	return (tmp);
 }
