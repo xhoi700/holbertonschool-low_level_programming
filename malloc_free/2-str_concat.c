36363cea506ceab480f40ed57fb8748801007cd9
xhoi700 <10057@holbertonstudents.com>
2024-11-12 09:19:25 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,30 +1,50 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
+
 /**
- * str_concat - function to concate 2 arry
+ * str_concat - function to concate 2 arrays
  * @s1: first string
  * @s2: second string
  * Return: null or a pointer to s
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	char *s;
+	char *result;
+	unsigned int i, j;
+	int size;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
-	s = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	if (s == NULL)
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+
+	size = i + j + 1;
+
+	result = malloc(size * sizeof(char));
+	if (result == NULL)
+	{
 		return (NULL);
+	}
 
-	for (i = 0; i < strlen(s1); i++)
-		s[i] = s1[i];
+	for (i = 0; s1[i] != '\0'; i++)
+	result[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+	result[i + j] = s2[j];
 
-	for (i = 0; i < strlen(s2); i++)
-		s[strlen(s1) + i] = s2[i];
-	s[strlen(s1) + i] = '\0';
-	return (s);
+	return (result);
+
 }
