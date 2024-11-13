3602d4a6ad3aa3caad369eb0873e9ce5d987afa1
xhoi700 <10057@holbertonstudents.com>
2024-11-13 12:51:49 +0000

    1-init_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,22 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - Short description
- * @name: First member
- * @age: Second member
- * @owner: Third member
- * Description: Longer description
+ * struct my_struct - Short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Define a new type struct dog
  */
 
-typedef struct dog
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+char *name;
+float age;
+char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif /* DOG_H *
+
+#endif
