f0ef2f800db0f473bca301d34d7213d7d0df1c61
xhoi700 <10057@holbertonstudents.com>
2024-11-13 12:32:00 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct my_struct - Short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Define a new type struct dog
+ */
+struct dog 
+{
+    char *name;
+    float age;
+    char *owner;
+};
