797a931a4caeeb6e6fe6a001f6a597c7b98eca3b
xhoi700 <10057@holbertonstudents.com>
2024-11-14 09:29:28 +0000

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,18 +1,17 @@
-#include<stdio.h>
-#include"dog.h"
+#include "dog.h"
+
 /**
- * init_dog -entry point,initializes a variable
- * @d: -points to specific memory
- * @name: -holds value for output
- * @age: -holds value for output
- * @owner: -holds value for output
+ *init_dog - awsd`scas
+ *@d: asdscas
+ *@name: adczxc
+ *@age: adczdc
+ *@owner: asdcadsc
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
