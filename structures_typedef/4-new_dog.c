dfe9394d5e949e12357258736bd479623cfe02d1
xhoi700 <10057@holbertonstudents.com>
2024-11-14 18:56:38 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,21 +10,25 @@
  * @owner: owner of the dog
  * Return: pointer to the new dog_t
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *sec_dog = malloc(sizeof(dog_t));
-	char *sec_name = malloc(strlen(name + 1));
-	char *sec_owner = malloc(strlen(owner + 1));
+	char *sec_name = malloc(strlen(name) + 1);
+	char *sec_owner = malloc(strlen(owner) + 1);
 
 	if (sec_dog == NULL || sec_name == NULL || sec_owner == NULL)
+	{
 		free(sec_dog);
 		free(sec_name);
 		free(sec_owner);
-	strcpy(dog_name, name);
-	strcpy(dog_owner, owner);
-	sec_dog->name = name;
-	sec_dog->owner = owner;
-	sec_dog->age = age;
+		return (NULL);
+	}
 
+	strcpy(sec_name, name);
+	strcpy(sec_owner, owner);
+	sec_dog->name = sec_name;
+	sec_dog->age = age;
+	sec_dog->owner = sec_owner;
 	return (sec_dog);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,23 +1,23 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
 
 /**
- * struct dog - Short description
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: Longer description
+ * struct dog -dog data
+ * @name: -points to specific data
+ * @age: -points to specific data
+ * @owner: -points to specific data
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
