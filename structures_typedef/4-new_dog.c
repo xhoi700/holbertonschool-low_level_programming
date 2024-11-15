16f478409daa3b01c0adfe9a3d72c7cd7eaec843
xhoi700 <10057@holbertonstudents.com>
2024-11-15 08:25:20 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,34 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: pointer to the new dog_t
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: NULL if function fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *sec_dog = malloc(sizeof(dog_t));
-	char *sec_name = malloc(strlen(name) + 1);
-	char *sec_owner = malloc(strlen(owner) + 1);
+	dog_t *my_dog;
+	int i, name_len, owner_len;
 
-	if (sec_dog == NULL || sec_name == NULL || sec_owner == NULL)
+	name_len = 0;
+	owner_len = 0;
+
+	for (i = 0; name[i] != '\0'; i++)
+		name_len++;
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_len++;
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (my_dog->name == NULL)
 	{
-		free(sec_dog);
-		free(sec_name);
-		free(sec_owner);
+		free(my_dog);
 		return (NULL);
 	}
-
-	strcpy(sec_name, name);
-	strcpy(sec_owner, owner);
-	sec_dog->name = sec_name;
-	sec_dog->age = age;
-	sec_dog->owner = sec_owner;
-	return (sec_dog);
+	my_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->age = age;
+	for (i = 0; name[i] != '\0'; i++)
+		my_dog->name[i] = name[i];
+	my_dog->name[i] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
+		my_dog->owner[i] = owner[i];
+	my_dog->owner[i] = '\0';
+	return (my_dog);
 }
