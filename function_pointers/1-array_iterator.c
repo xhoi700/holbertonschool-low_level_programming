e638e74b865bbf02a3710c3c3da7887f21ffcc6a
xhoi700 <10057@holbertonstudents.com>
2024-11-18 10:54:02 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,13 @@
 #include "function_pointers.h"
 #include <stddef.h>
 
+/**
+* array_iterator - entry point,executes a function
+* @array: -points to the respective array
+* @size: -holds value for output
+* @action: -points to the respective funct
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     size_t i;
