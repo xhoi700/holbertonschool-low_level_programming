6b16c64d3a766c06cf8792b98b4cd66bbb72b181
xhoi700 <10057@holbertonstudents.com>
2024-11-18 10:52:37 +0000

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    size_t i;
+
+    if (array != NULL && action != NULL)
+    {
+        for (i = 0; i < size; i++)
+        {
+            action(array[i]);
+
+        }
+    }
+}
